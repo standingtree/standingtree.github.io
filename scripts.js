e4d0d1a02201f9954a73cc79c017d00307c9988b
Viren Desai <virendesai87@gmail.com>
2023-01-05 14:15:37 -0600

    minor edits

--- scripts.js
+++ scripts.js
@@ -62,7 +62,7 @@ projectList.push({
 })
 
 projectList.push({
-    title: "Paani Data [**PRIVATE** Please contact me directly for access]",
+    title: "Paani Data [**PRIVATE** Please contact me directly for a demo]",
     desc: "A curation of public data on the Indian water industry.",
     repoName: "paani-data"
 })
