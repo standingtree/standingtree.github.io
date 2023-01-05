52d4a7e1f9211c94acadb6ecc76e50637867cc29
Viren Desai <virendesai87@gmail.com>
2023-01-05 14:14:01 -0600

    minor edits

--- scripts.js
+++ scripts.js
@@ -57,7 +57,7 @@ let populateProjects = () => {
 
 projectList.push({
     title: "Real-Time MBTA Bus Tracker (Route 1)",
-    desc: "An exercise in working with real-time web data from a third party API, and map manipulation in the DOM using the Mapbox API library.",
+    desc: "Current locations of MBTA Route 1 buses in Boston, using real-time web data from a third party API and map manipulation in the DOM using the Mapbox API library.",
     repoName: "real-time-bus-tracker"
 })
 
